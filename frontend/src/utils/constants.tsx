f48ee9242f475bbad9922e61515a456afa4c7b80
Marian-Janolek <m.janolek@gmail.com>
2022-02-27 11:05:42 +0100

    add refactor of botton bar

--- frontend/src/utils/constants.tsx
+++ frontend/src/utils/constants.tsx
@@ -2,9 +2,9 @@ import { FaPencilAlt, FaEdit, FaTimes, FaRegLightbulb } from 'react-icons/fa';
 import { GrDocumentPdf } from 'react-icons/gr';
 
 export const bottonBarIcons = [
-  { id: 1, text: 'Mark', icon: <FaPencilAlt /> },
-  { id: 2, text: 'Edit', icon: <FaEdit /> },
-  { id: 3, text: 'Mark', icon: <FaTimes /> },
-  { id: 4, text: 'Hint', icon: <FaRegLightbulb /> },
-  { id: 5, text: 'PDF', icon: <GrDocumentPdf /> },
+  { id: 1, icon: <FaPencilAlt />, state: '1' },
+  { id: 2, icon: <FaEdit />, state: '0' },
+  { id: 3, icon: <FaTimes />, state: 'X' },
+  { id: 4, icon: <FaRegLightbulb /> },
+  { id: 5, icon: <GrDocumentPdf /> },
 ];
